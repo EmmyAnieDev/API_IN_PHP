aedf9410df9a824e4318d68a89c6a0fd205f0143
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-10-17 08:27:33 +0100

    Decode Api Results: reading JSON in PHP

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
     // it can also be used to retrieve the contennt of a website
     $response = file_get_contents("https://randomuser.me/api");
 
-    echo $response;
+    // Decode the received data into a standard format. Set true to convert it into an associative array.
+    $data = json_decode($response, true);
+
+    // var_dump($data); "\n";
+
+    // print only the first name of the returned user using the array key
+    echo $data['results'][0]['name']['first'], "\n";
 
 ?>
