c1e491abfbfcc757320915cd7629598b1cdcf9c6
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-10-18 18:22:41 +0100

    use an API that requires a specific request header

--- index.php
+++ index.php
@@ -9,39 +9,17 @@ $ch = curl_init();
 
 # Headers represent metadata about the request, such as authentication credentials, 
 # information about the client, or the type of content being sent or requested.
-
-// You can add as many headers as needed, depending on the API's requirements.
 $headers = [
-    "Authorization: Client-ID YOUR_ACCESS_KEY"
+    "Authorization: token YOUR_ACCESS_KEY",
+    "User-Agent: EmmyAnieDev"
 ];
-
-$response_headers = [];  // Initialize an empty array to store response headers.
-
-// Define a callback function to process headers received in the response.
-$header_callback = function($ch, $header) use (&$response_headers) {
-
-    $len = strlen($header);  // Get the length of the current header line.
-
-    $parts = explode(':', $header, 2);  // Split the header into two parts: the header name and its value.
-
-    if (count($parts) < 2) {  // If the header doesn't contain a colon (i.e., it's not a valid header), skip it.
-        return $len;  // Return the length of the header (required for the cURL callback to continue).
-    }
-
-    $response_headers[$parts[0]] = trim($parts[1]);  // Store the header name (key) and its value (after trimming spaces) in the $response_headers array.
-
-    return $len;  // Return the length of the processed header line to let cURL know to continue processing headers.
-};
-
 
 
 curl_setopt_array($ch, [
 
-    // CURLOPT_URL => "https://api.unsplash.com/photos/random?client_id=YOUR_ACCESS_KEY",
-    CURLOPT_URL => "https://api.unsplash.com/photos/random",
+    CURLOPT_URL => "https://api.github.com/user/starred/httpie/httpie",
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_HTTPHEADER => $headers,
-    CURLOPT_HEADERFUNCTION => $header_callback
 
 
 ]);
@@ -53,8 +31,6 @@ $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch); 
 
 echo $status_code . "\n";
-
-print_r($response_headers);
 
 print_r($respone . "\n"); 
 
