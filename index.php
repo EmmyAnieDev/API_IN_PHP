e2d52aab5d77f2cbf37f42c74ba94ea83ae5ae3f
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-10-18 20:44:51 +0100

    Use the Guzzle HTTP client for object-oriented API code

--- index.php
+++ index.php
@@ -1,37 +1,39 @@
 <?php
 
-error_reporting(E_ALL); 
-ini_set('display_errors', 1); 
 
-// Initialize a cURL session
-$ch = curl_init();
+/* 
+Line 1: This includes the Composer autoload file, which automatically loads the GuzzleHttp classes 
+and other dependencies.
+*/
+require __DIR__ . "/vendor/autoload.php"; 
 
-$headers = [
-    "User-Agent: EmmyAnieDev"
-];
 
-curl_setopt_array($ch, [
+// Line 2: Creates a new Guzzle HTTP client object that will be used to make the HTTP request.
+$client = new GuzzleHttp\Client;
 
-    CURLOPT_URL => "https://api.github.com/gists/654ef2658142cc48859dad324e827f9e", 
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_HTTPHEADER => $headers,
+
+/*
+ Line 3: Sends a GET request to GitHub's API to retrieve the user's repositories. 
+ The "request" method sends the request and stores the response in the $response variable.
+ */
+$response = $client->request("GET", "https://api.github.com/user/repos", [
+
+    "headers" => [
+        // Line 5: Authorization header with a GitHub personal access token to authenticate the request.
+        "Authorization" => "token YOUR_TOKEN", 
+        // Line 6: A "User-Agent" header is required by GitHub API to identify the client making the request.
+        "User-Agent" => "EmmyAnieDev"
+    ]
 
 ]);
 
-$respone = curl_exec($ch);
-
-curl_close($ch); 
-
-$data = json_decode($respone, true);
-
-/*
-foreach ($data as $gist) {
-
-    echo $gist['id'], " - ", $gist['description'], "\n";
-
-}
-*/
-
-print_r($data);
+// Line 10: Outputs the HTTP status code from the response (e.g., 200 for success).
+echo $response->getStatusCode(), "\n";
 
 
+//Line 12: Outputs the 'content-type' of the response header (e.g., application/json), 
+//which indicates the format of the response.
+echo $response->getHeader("content-type")[0], "\n";
+
+// Line 14: Outputs the first 200 characters of the response body to preview the data retrieved from the API.
+echo substr($response->getBody(), 0, 200), "...\n";
