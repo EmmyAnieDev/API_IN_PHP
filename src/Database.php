797e38cdf30fa9637a8e6ed7f8ac88c94ae505b3
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-10-27 19:40:29 +0100

    Cache the database connection to avoid multiple connnections in the same request

--- src/Database.php
+++ src/Database.php
@@ -2,21 +2,38 @@
 
 class Database {
 
+    private ?PDO $conn = null;
+
     public function __construct(
-            private string $host,
-            private string $name,
-            private string $user,
-            private string $password,){}
+        private string $host,
+        private string $name,
+        private string $user,
+        private string $password
+    ) {}
 
-    public function getConnection(): PDO{
+    public function getConnection(): PDO {
+        
+        if ($this->conn === null) {
+            $dsn = "mysql:host={$this->host};dbname={$this->name};charset=utf8";
+            
+            try {
+                // Establish a new PDO connection
+                $this->conn = new PDO($dsn, $this->user, $this->password, [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_EMULATE_PREPARES => false,
+                    PDO::ATTR_STRINGIFY_FETCHES => false
+                ]);
+            } catch (PDOException $e) {
+                
+                throw new RuntimeException("Database connection error: " . $e->getMessage());
+            }
+        }
+        
+        return $this->conn;
+    }
 
-        $dsn = "mysql:host={$this->host};dbname={$this->name}; charset=utf8";
-
-        return new PDO($dsn, $this->user, $this->password, [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_EMULATE_PREPARES => false,
-            PDO::ATTR_STRINGIFY_FETCHES => false
-        ]);
-
+    
+    public function closeConnection(): void {
+        $this->conn = null;
     }
 }
