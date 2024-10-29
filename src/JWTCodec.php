5a7d0b1c6e310fbcf58e1bf7f9301df6acb6efc4
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-10-30 00:24:21 +0100

    Added a method to decode the payload from the JWT

--- src/JWTCodec.php
+++ src/JWTCodec.php
@@ -1,5 +1,7 @@
 <?php
 
+use Dotenv\Exception\InvalidEncodingException;
+
 class JWTCodec {
 
     // Encodes the payload into a JWT string with a header, payload, and signature
@@ -20,10 +22,42 @@ class JWTCodec {
 
     }
 
+    // This decode function decodes a JWT token, verifies its format and signature, and returns the payload if valid.
+    public function decode($token) : array {
+
+        if (preg_match("/^(?<header>.+)\.(?<payload>.+)\.(?<signature>.+)$/", $token, $matches) !==1) {
+
+            throw new InvalidArgumentException("Invalid token format");
+
+        }
+
+        $signature = hash_hmac("sha256", $matches['header'] . "." . $matches['payload'], "lz7CM2jVTIDkSUNIdwWSfdDs9xJ9gqQhyHaPtH6xoe4=", true);
+        $signature_from_token = $this->base64urlDecode($matches['signature']);
+        
+        if ( !hash_equals($signature, $signature_from_token)) {
+
+            throw new Exception("signature doesn't match!");
+
+        }
+
+        $payload = json_decode($this->base64urlDecode($matches['payload']), true);
+
+        return $payload;
+
+    }
+
     // Encodes a string in base64 URL format
     public function base64urlEncode(string $text) : string {
 
         return str_replace(["+", "/", "="], ["-", "_", ""], base64_encode($text));
+
+    }
+
+    // Decodes a base64 URL-encoded string
+    public function base64urlDecode(string $text) : string {
+
+        $base64 = str_replace(["-", "_"], ["+", "/"], $text);
+        return base64_decode($base64);
 
     }
 
